0d13b41a5b02fb8585f0050e2542c7adb86b102a
losper <260251708@qq.com>
2019-04-14 11:18:01 +0800

    fiexed .gitignore

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+  ((projectile-project-compilation-cmd . "cd build && pwd && sh gcc-linux.sh"))))
+
--- test/index.cpp
+++ test/index.cpp
@@ -5,11 +5,11 @@
 
 using namespace cactus;
 
-TEST_CASE("Factorials are computed", "[factorial]") {
-  Tensor<float> a;
-  a = {1, 2, 3};
-  auto z = a * 2;
-  REQUIRE(z.ref({0}) == 2);
-  REQUIRE(z.ref({1}) == 4);
-  REQUIRE(z.ref({2}) == 6);
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    Tensor<float> a;
+    a      = {1, 2, 3};
+    auto z = a * 2;
+    REQUIRE( z.ref( {0} ) == 2 );
+    REQUIRE( z.ref( {1} ) == 4 );
+    REQUIRE( z.ref( {2} ) == 6 );
 }
